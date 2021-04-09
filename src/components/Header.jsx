af6e9b29f5180b3f53ed2e19017da0f4fe87c3b7
pcProgramate <paolaroa1925@gmail.com>
2021-04-09 10:12:07 -0500

    set header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import {
     BrowserRouter as Router,
-    Link, 
+    Link,
     Switch,
     Route
-    }
-  from 'react-router-dom';
+}from 'react-router-dom';
+import logo from '../assets/images/logoPR.png';
+import '../assets/css/menu.css';
 
 const Header = () => {
-    return ( 
-        <header>
-            <ul>
-                <li>
-                    <Link to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link to="/about">Sobre mí</Link>
-                </li>
-                <li>
-                    <Link to="/portfolio">Portafolio</Link>
-                </li>
-                <li>
-                    <Link to="/services">Servicios</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contáctame</Link>
-                </li>
-            </ul>
-        </header>
+    return (
+
+        <div class="menu">
+            <a href="#inicio" id="logo-menu">
+                <img class="logo" src={logo} alt="" />
+            </a>
+            <header>
+                <input type="checkbox" id="btn-menu" />
+                <label for="btn-menu"><i class="fas fa-bars"></i></label>
+                <nav class="mainmenu">
+                    <ul>
+                        <li>
+                            <Link to="/">Inicio</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Sobre mí</Link>
+                        </li>
+                        <li>
+                            <Link to="/portfolio">Portafolio</Link>
+                        </li>
+                        <li>
+                            <Link to="/services">Servicios</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contáctame</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
 
      );
 }
